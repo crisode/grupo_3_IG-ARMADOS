370055737cfe2abc5448e8b49bc76a9e0c5445c2
Lautaro Valdez <lautarovaldez445@gmail.com>
2021-01-27 14:03:56 -0300

    Borre sin queres el app.use de producto jaja

--- IG-ARMADOS/app.js
+++ IG-ARMADOS/app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/carrito", carritoRouter);
+app.use("/producto", productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
