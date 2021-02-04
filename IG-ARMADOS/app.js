b36cb2afcac030bcd75d9b007aaa28ce1c7d8042
Lautaro Valdez <lautarovaldez445@gmail.com>
2021-02-04 20:04:57 -0300

    Se inicio la pag carga de Producto

--- IG-ARMADOS/app.js
+++ IG-ARMADOS/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/productRouter');
 var carritoRouter = require("./routes/carritoRouter");
+var adminRouter = require("./routes/adminRouter");
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/carrito", carritoRouter);
 app.use("/producto", productRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
