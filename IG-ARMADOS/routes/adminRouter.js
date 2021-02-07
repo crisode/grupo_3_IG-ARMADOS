2c72145fd5676de1b58008b89f89d098de7c6289
KarenJunco <karenbetianajunco@hotmail.com>
2021-02-07 18:06:41 -0300

    agregue el home y carga de producto

--- IG-ARMADOS/routes/adminRouter.js
+++ IG-ARMADOS/routes/adminRouter.js
@@ -2,7 +2,7 @@ const adminController = require("../controllers/adminController")
 var express = require('express');
 var router = express.Router();
 
-router.get("/carga", adminController.cargaProducto)
+router.get("/", adminController.cargaProducto)
 
 
 module.exports = router;
--- IG-ARMADOS/routes/index.js
+++ IG-ARMADOS/routes/index.js
@@ -1,9 +1,8 @@
+const indexController = require("../controllers/indexController")
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", indexController.index)
+
 
 module.exports = router;
