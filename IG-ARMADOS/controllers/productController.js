a6b4d08907824ce5f77a9c22097f368d276856bc
crisode <voyporlomejor@gmail.com>
2021-02-19 01:52:28 -0300

    se agrega template detalle producto

--- IG-ARMADOS/controllers/productController.js
+++ IG-ARMADOS/controllers/productController.js
@@ -1,6 +1,21 @@
+const { getProducts } = require('../data/products')
+const productos = getProducts(); 
 
 module.exports = {
     detalle: (req, res) => {
-        res.render("productoDetalle")
+
+        let producto = productos.find(producto => {
+            return producto.id === Number(req.params.id)
+        });
+        let novedades = productos.filter(producto => {
+            return producto.category == "novedades"
+        });
+
+        res.render("productoDetalle",{
+            producto,
+            novedades
+
+
+        })
     }
 }
--- IG-ARMADOS/routes/productRouter.js
+++ IG-ARMADOS/routes/productRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {detalle} = require("../controllers/productController")
 
-router.get('/', detalle);
+
+router.get("/:id",detalle);
 
 module.exports = router;
