b57045817f5f1fe0c7e5b04a03d46f83c2f56953
tabitoo <lautarovaldez445@gmail.com>
2021-03-01 20:23:39 -0300

    retoques de cookies y actualizacion de session

--- IG-ARMADOS/routes/index.js
+++ IG-ARMADOS/routes/index.js
@@ -1,8 +1,9 @@
 const { index, contact } = require('../controllers/indexController');
 var express = require('express');
 var router = express.Router();
+const localCheck = require("../middlewares/localCheck");
 
-router.get("/", index);
+router.get("/", localCheck,index);
 router.get("/contact", contact);
 
 
