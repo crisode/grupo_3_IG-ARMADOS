2923071fa2233886c6d8871670339c2c8f5776bb
tabitoo <lautarovaldez445@gmail.com>
2021-03-01 20:26:13 -0300

    retoques de cookies

--- IG-ARMADOS/middlewares/cookieCheck.js
+++ IG-ARMADOS/middlewares/cookieCheck.js
@@ -1,6 +1,6 @@
 module.exports= ( req, res, next)=>{
-    if (req.cookies.userLogin) {                               
-        req.session.user = req.cookies.userLogin
+    if (req.cookies.userCom4) {                               
+        req.session.user = req.cookies.userCom4
     }
     next()
 }
