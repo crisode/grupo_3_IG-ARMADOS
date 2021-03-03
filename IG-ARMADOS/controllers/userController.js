57b5eb4134f1c5f4885b40dcb6ec11feb963d99d
ignacioirigoitia <ignacio.irigoitia@gmail.com>
2021-03-03 16:53:40 -0300

    arregle cookies

--- IG-ARMADOS/controllers/userController.js
+++ IG-ARMADOS/controllers/userController.js
@@ -48,8 +48,8 @@ module.exports = {
 
                     // creo la cookie para cuando el usuario elija recordarme
                     
-                    if (recordar != "undefined") {
-                        res.cookie("userCom4", req.session.user, { maxAge: 1000 * 60 * 60 * 24 }); // 1 dia de recordar la cookie
+                    if(recordar){
+                        res.cookie("userCom4", req.session.user, {maxAge: 1000 * 60 * 60 * 24}); // 1 dia de recordar la cookie
                     }
 
                     return res.redirect("/");
