d4b725cc1e31163756b2342ef5793586d3ccb92b
tabitoo <lautarovaldez445@gmail.com>
2021-03-15 18:23:51 -0300

    Controladores de producto e index actualizados

--- IG-ARMADOS/controllers/productController.js
+++ IG-ARMADOS/controllers/productController.js
@@ -1,12 +1,56 @@
-const { getProducts } = require('../data/products')
-const productos = getProducts(); 
+
+const db = require('../database/models');
+
+
 
 module.exports = { 
     detalle: (req, res) => {
 
+        db.Producto.findOne({
+            where : {
+                id : req.params.id
+            },
+            include : [
+                {
+                    association : 'carrito'
+                },
+                
+                {
+                    association : 'categoria'
+                },
+                {
+                    association : 'componente'
+                },
+                {
+                    association : 'marca'
+                },
+                {
+                    association : 'imagen'
+                },
+                {
+                    association : 'garantia'
+                }
+            ]
+        })
+        .then(producto => {
+            let novedades = producto.filter(producto => {
+                return producto.categoria.name == "novedades"
+            });
+            res.render("productoDetalle",{
+                title:"Detalle",
+                producto,
+                novedades
+            })
+        }).catch(error => console.log(error))
+        
+
+
+
+        /*
         let producto = productos.find(producto => {
             return producto.id === Number(req.params.id)
         });
+        
         let novedades = productos.filter(producto => {
             return producto.category == "novedades"
         });
@@ -15,7 +59,8 @@ module.exports = {
             title:"Detalle",
             producto,
             novedades
-        })
+        })*/
+
     },
     carrito :(req, res)=>{
         res.render("carrito",{
