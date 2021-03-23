3def2b77f40af4a12a2fa28ffffaccea5676eec7
ignacioirigoitia <ignacio.irigoitia@gmail.com>
2021-03-23 14:42:42 -0300

    agregue archivos JS y agregue spans a las vistas

---
+++ IG-ARMADOS/public/javascript/crearProducto.js
@@ -0,0 +1,36 @@
+let qs = function (element) {
+    return document.querySelector(element);
+  };
+
+window.addEventListener("load", () => {
+    console.log("Vinculado!")
+
+    let $errorImagen = qs("#errorImagen"),
+        $errorTitulo = qs("#errorTitulo"),
+        $errorPrecio = qs("#errorPrecio"),
+        $errorOferta = qs("#errorOferta"),
+        $errorGarantia = qs("#errorGarantia"),
+        $errorComponente = qs("#errorComponente"),
+        $errorMarca = qs("#errorMarca"),
+        $errorCategoria = qs("#errorCategoria"),
+        $errorModelo = qs("#errorModelo"),
+        $errorStock = qs("#errorStock"),
+        $errorDescripcion = qs("#errorDescripcion"),
+        $errorCaracteristicas = qs("#errorCaracteristicas"),
+        $errorBoton = qs("#errorBoton"),
+        $botonEnviar = qs("#botonEnviar"),
+        $imagen = qs("#imagen"),
+        $titulo = qs("#titulo"),
+        $precio = qs("#precio"),
+        $oferta = qs("#oferta"),
+        $garantia = qs("#garantia"),
+        $componente = qs("#componente"),
+        $marca = qs("#marca"),
+        $categoria = qs("#categoria"),
+        $modelo = qs("#modelo"),
+        $stock = qs("#stock"),
+        $descripcion = qs("#descripcion"),
+        $caracteristicas = qs("#caracteristicas"),
+        $imagenPreview = qs("#imagenPreview")
+
+});
---
+++ IG-ARMADOS/public/javascript/editarProducto.js
@@ -0,0 +1,32 @@
+let qs = function (element) {
+  return document.querySelector(element);
+};
+
+window.addEventListener("load", () => {
+  let $errorImagen = qs("#errorImagen"),
+    $errorTitulo = qs("#errorTitulo"),
+    $errorPrecio = qs("#errorPrecio"),
+    $errorOferta = qs("#errorOferta"),
+    $errorGarantia = qs("#errorGarantia"),
+    $errorComponente = qs("#errorComponente"),
+    $errorMarca = qs("#errorMarca"),
+    $errorCategoria = qs("#errorCategoria"),
+    $errorModelo = qs("#errorModelo"),
+    $errorStock = qs("#errorStock"),
+    $errorDescripcion = qs("#errorDescripcion"),
+    $errorCaracteristicas = qs("#errorCaracteristicas"),
+    $errorBoton = qs("#errorBoton"),
+    $botonEnviar = qs("#botonEnviar"),
+    $imagen = qs("#imagen"),
+    $titulo = qs("#titulo"),
+    $precio = qs("#precio"),
+    $oferta = qs("#oferta"),
+    $garantia = qs("#garantia"),
+    $componente = qs("#componente"),
+    $marca = qs("#marca"),
+    $categoria = qs("#categoria"),
+    $modelo = qs("#modelo"),
+    $stock = qs("#stock"),
+    $descripcion = qs("#descripcion"),
+    $caracteristicas = qs("#caracteristicas");
+});
