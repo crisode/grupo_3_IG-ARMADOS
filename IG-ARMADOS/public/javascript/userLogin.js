96ddbce84d67d7577550c1d48f643e13ab97b726
ignacioirigoitia <ignacio.irigoitia@gmail.com>
2021-04-06 15:34:12 -0300

    agregue evento furmulario login

--- IG-ARMADOS/public/javascript/userLogin.js
+++ IG-ARMADOS/public/javascript/userLogin.js
@@ -1,58 +1,81 @@
-let qs = function(element) {
+let qs = function (element) {
     return document.querySelector(element)
-
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 
 
     let $inputEmail = qs('#email'),
-    $emailErrors = qs('#emailErrors'),
-    $Pass = qs('#pass'),
-    $passErrors = qs('#passErrors'),
-    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-    regExPass = /^.{6,12}$/;
+        $emailErrors = qs('#emailErrors'),
+        $Pass = qs('#pass'),
+        $passErrors = qs('#passErrors'),
+        regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+        regExPass = /^.{6,12}$/,
+        $formLogin = qs("#formLogin"),
+        $errorForm = qs("#errorForm")
 
-/* VALIDACIÓN EMAIL */
+    /* VALIDACIÓN EMAIL */
 
-$inputEmail.addEventListener('blur', function(){
-    switch (true) {
-        case !$inputEmail.value.trim():
-            $emailErrors.innerHTML = 'El campo email es obligatorio';
-            $inputEmail.classList.add('invalido')
-            break;
-        case !regExEmail.test($inputEmail.value):
-            $emailErrors.innerHTML = 'Debes ingresar un Email válido'
-            $inputEmail.classList.add('invalido')
-            break;
-        default:
-            $inputEmail.classList.remove('invalido');
-            $inputEmail.classList.add('valido');
-            $emailErrors.innerHTML = ''
-            break;
-    }
+    $inputEmail.addEventListener('blur', function () {
+        switch (true) {
+            case !$inputEmail.value.trim():
+                $emailErrors.innerHTML = 'El campo email es obligatorio';
+                $inputEmail.classList.add('invalido')
+                break;
+            case !regExEmail.test($inputEmail.value):
+                $emailErrors.innerHTML = 'Debes ingresar un Email válido'
+                $inputEmail.classList.add('invalido')
+                break;
+            default:
+                $inputEmail.classList.remove('invalido');
+                $inputEmail.classList.add('valido');
+                $emailErrors.innerHTML = ''
+                break;
+        }
 
-})
+    })
 
-/* VALIDACIÓN PASSWORD  */
+    /* VALIDACIÓN PASSWORD  */
 
-$Pass.addEventListener('blur', function(){
-    switch (true) {
-        case !$Pass.value.trim():
-            $passErrors.innerHTML = 'El campo contraseña es obligatorio.';
-            $Pass.classList.add('invalido')
-            break;
-        case !regExPass.test($Pass.value):
-            $passErrors.innerHTML = 'Debe tener entre 6 y 12 caracteres'
-            $Pass.classList.add('invalido')
-            break;
-        default:
-            $Pass.classList.remove('invalido');
-            $Pass.classList.add('valido');
-            $passErrors.innerHTML = ''
-            break;
-    }
+    $Pass.addEventListener('blur', function () {
+        switch (true) {
+            case !$Pass.value.trim():
+                $passErrors.innerHTML = 'El campo contraseña es obligatorio.';
+                $Pass.classList.add('invalido')
+                break;
+            case !regExPass.test($Pass.value):
+                $passErrors.innerHTML = 'Debe tener entre 6 y 12 caracteres'
+                $Pass.classList.add('invalido')
+                break;
+            default:
+                $Pass.classList.remove('invalido');
+                $Pass.classList.add('valido');
+                $passErrors.innerHTML = ''
+                break;
+        }
 
-})
+    })
+
+    /* VALIDACION FORMULARIO */
+
+    $formLogin.addEventListener("submit", function (event) {
+        let error = false;
+        event.preventDefault()
+        console.log($form.elements)
+        let elementosForm = this.elements
+
+        for (let index = 0; index < 2; index++) {
+            if (elementosForm[index].value = "") {
+                elementosForm[index].classList.add("invalido")
+                $errorForm.innerHTML = "Los campos señalados son obligatorios"
+                $errorForm.classList.add("errorFormulario")
+                error = true;
+            }
+        }
+
+        if (!error) {
+            $form.submit()
+        }
+    })
 
 })
